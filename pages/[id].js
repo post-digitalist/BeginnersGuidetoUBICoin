34e9fc49e6d05ae5a21c9351f0cc3e5d144df44d
Pablo Toledo Margalef <pabloatm980@gmail.com>
2021-02-18 12:20:58 -0300

    Rename props

--- pages/[id].js
+++ pages/[id].js
@@ -2,16 +2,17 @@ import Layout from "../components/layout";
 import {
   getContentData,
   getNextSlide,
+  getPrevSlide,
   getAllContentIds,
   getSortedContentData,
 } from "../lib/data";
 import Head from "next/head";
 import Arrow from "../components/arrow";
 
-export default function Slide({ currentId, nextId, slideData, slideLinks }) {
+export default function Slide({ currentId, nextSlide, slideData, slideLinks }) {
   const {
     params: { id: nextSlideId },
-  } = nextId;
+  } = nextSlide;
   return (
     <Layout footerLinks={slideLinks}>
       <Head>
@@ -37,12 +38,15 @@ export const getStaticProps = async ({ params }) => {
   const slides = getSortedContentData();
   const slideData = await getContentData(params.id);
   const currentId = params.id;
-  const nextId = await getNextSlide(params.id);
+  const nextSlide = await getNextSlide(params.id);
+  const prevSlide = await getPrevSlide(params.id);
+
   return {
     props: {
       currentId,
       slideData,
-      nextId,
+      nextSlide,
+      prevSlide,
       slideLinks: slides.map(({ title, id }) => ({ id, title })),
     },
   };
